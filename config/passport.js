b18e2df53ff5bfbdd6fe393272bd469dba75ae68
alexmcdermid <alexander.mcdermid@gmail.com>
2021-10-06 15:09:12 -0700

    slider working

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,8 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const Review = require('../models/reviews');
+const Student = require('../models/student');
+
 
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
@@ -8,16 +10,38 @@ passport.use(new GoogleStrategy({
     callbackURL: process.env.GOOGLE_CALLBACK
   },
   function(accessToken, refreshToken, profile, cb) {
-    // a user has logged in with OAuth...
+    Student.findOne({ 'googleId': profile.id }, function(err, student) {
+      if (err) return cb(err);
+      if (student) {
+        return cb(null, student);
+      } else {
+        // we have a new student via OAuth!
+        var newStudent = new Student({
+          name: profile.displayName,
+          email: profile.emails[0].value,
+          googleId: profile.id
+        });
+        console.log('this is a new student'+newStudent)
+        createStudent(newStudent)
+        newStudent.save(function(err) {
+          if (err) return cb(err);
+          return cb(null, newStudent);
+        });
+      }
+    });
   }
 ));
 
-passport.serializeUser(function(Review, done) {
-    done(null);
+passport.serializeUser(function(student, done) {
+    done(null, student.id);
 });
 
-// passport.deserializeUser(function(id, done) {
-//     Student.findById(id, function(err, student) {
-//       done(err, student);
-//     });
-//   });
+passport.deserializeUser(function(id, done) {
+    Student.findById(id, function(err, student) {
+      done(err, student);
+    });
+  });
+
+async function createStudent(newStudent) {
+    Student.create(newStudent)
+}
