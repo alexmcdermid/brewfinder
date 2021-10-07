cdc973ec3c10efe9d97ffc26bc077af7c9423a17
alexmcdermid <alexander.mcdermid@gmail.com>
2021-10-07 14:01:13 -0700

    limit reviews to 1 per person

--- config/passport.js
+++ config/passport.js
@@ -9,7 +9,6 @@ passport.use(new GoogleStrategy({
     callbackURL: process.env.GOOGLE_CALLBACK
   },
   function(accessToken, refreshToken, profile, cb) {
-    console.log("Google's info:", profile)
     User.findOne({ 'googleId': profile.id }, function(err, user) {
       if (err) return cb(err);
       if (user) {
